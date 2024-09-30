ab14451ccbeafa15abd26e84c810dc59b8abcbbb
MohammedSinancholakkal <sinansibi@gmail.com>
2024-09-30 13:05:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function search(){
+    const age = document.getElementById('age').value;
+    const high = document.getElementById('high').value;
+    const weight = document.getElementById('weight').value;
+
+    if(age == "" || high == "" || weight == ""){
+        alert("fill the columns")
+    }else{
+        const bmi = Math.ceil(weight / ((high / 100) ** 2));
+
+        if (bmi < 18.5){
+            answer.innerHTML=`
+           <h2 class=" text-center text-danger">${bmi}kg</h2> <h3 class="text-center text-primary"> under weight</h3>
+
+            `
+        }
+        else if(bmi <25){
+            answer.innerHTML=`<h2 class=" text-center text-danger">${bmi}kg</h2> <h3 class="text-center text-success"> normal weight</h3> `
+        }
+        else if(bmi <35){
+            answer.innerHTML=`<h2 class=" text-center text-danger">${bmi}kg</h2> <h3 class="text-center text-danger"> over weight</h3>`
+        }
+        else{
+            answer.innerHTML=`<h2 class=" text-center text-danger">${bmi}kg</h2> <h3 class="text-center text-danger">you Are obese </h3>`
+
+        }
+    }
+
+}
+
+function allclear(){
+
+
+const age = document.getElementById('age').value = ""
+const high = document.getElementById('high').value = ""
+const weight = document.getElementById('weight').value = ""
+answer.innerHTML= ""
+
+}
